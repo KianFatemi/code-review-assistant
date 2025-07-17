907e4f00f5272921a74e56e23a862cbb4535d2d9
KianFatemi <kianfatemi044@gmail.com>
2025-07-17 12:59:47 -0400

    first commit

---
+++ code-review-assistant/src/main/java/io/github/kianfatemi/code_review_assistant/CodeReviewAssistantApplication.java
@@ -0,0 +1,13 @@
+package io.github.kianfatemi.code_review_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewAssistantApplication.class, args);
+	}
+
+}
---
+++ code-review-assistant/src/test/java/io/github/kianfatemi/code_review_assistant/CodeReviewAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kianfatemi.code_review_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
